4db43d5d0056f207601e416d9ff497884817f7fc
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-14 19:02:09 +0200

    fix(tests): rewrited render according to redux doc

--- src/test-utils/index.tsx
+++ src/test-utils/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render as rtlRender } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { store } from 'store';
+import reducer from 'store/reducer';
 
-const ReduxProvider: React.FC = ({ children }) => {
-  return (
-    <Provider store={store}>
-      <Router>
-        {children}
-      </Router>
-    </Provider>
-  );
-};
-
-const reduxRender = (children: React.ReactNode) => {
-  return render(<ReduxProvider>{children}</ReduxProvider>);
-};
+function render(
+  ui: React.ReactElement,
+  {
+    store = configureStore({ reducer: reducer }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }: { children: React.ReactNode }) {
+    return (
+      <Provider store={store}>
+        <Router>
+          {children}
+        </Router>
+      </Provider>
+    );
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
 
 export * from '@testing-library/react';
-
-export { reduxRender as render };
+export { render };
