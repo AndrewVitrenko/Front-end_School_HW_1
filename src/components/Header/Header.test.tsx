025ac29bda1767653e5cf3ba3b4a35dc1f229f4b
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-15 14:04:56 +0200

    test(header): wrote tests for header

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent } from 'test-utils';
+import { Header } from '.';
+
+describe('Header', () => {
+  
+  it('should render properly', () => {
+    render(<Header />);
+    expect(screen.getByText('Feed')).toBeInTheDocument();
+  });
+
+  it('should redirect with button links', () => {
+    render(<Header />);
+    fireEvent.click(screen.getByText('Feed'));
+    expect(window.location.href).toEqual('http://localhost/feed');
+
+    fireEvent.click(screen.getByText('Profile'));
+    expect(window.location.href).toEqual('http://localhost/profile');
+  });
+});
