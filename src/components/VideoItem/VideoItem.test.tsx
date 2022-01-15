a873bff900fb933893717d9136aafcd7b8ff22a2
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-15 15:17:25 +0200

    test(videoItem): wrote tests for video item

---
+++ src/components/VideoItem/VideoItem.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from 'test-utils';
+import { VideoItem } from '.';
+import { Video } from 'interfaces';
+
+describe('VideoItem', () => {
+  let video: Video;
+
+  beforeEach(() => video = {
+    id: '1',
+    stats: {
+      playCount: 555
+    },
+    video: {
+      playAddr: 'test url'
+    }
+  });
+
+  it('should render properly', () => {
+    render(<VideoItem video={video} />);
+    expect(screen.getByText(`Played: ${video.stats.playCount} times!`)).toBeInTheDocument();
+  });
+})
