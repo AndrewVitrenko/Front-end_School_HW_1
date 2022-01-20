b48a2c2c7723d046ec036fb6ab862837ddd2f43a
Andrew Vitrenko <avitrenko2004@gmail.com>
2022-01-20 17:12:10 +0200

    feat: created app directory

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Header } from 'components/Header';
 import { Main } from 'components/Main';
+import './index.css';
 
-
-function App() {
-
+const App: React.FC = () => {
   return (
     <Router>
       <Header />
