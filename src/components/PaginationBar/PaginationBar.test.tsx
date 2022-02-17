f0465e33303c16f8b4f44673d9149cc718c74681
Andriy Vitrenko <andriy.v@codegenerationsoftware.com>
2022-02-17 23:33:09 +0200

    made assigning props in beforeEach

--- src/components/PaginationBar/PaginationBar.test.tsx
+++ src/components/PaginationBar/PaginationBar.test.tsx
@@ -1,39 +1,40 @@
-import { ChangeEvent } from 'react';
-import { render, screen, fireEvent } from 'test-utils';
-import { PaginationBar } from '.';
+import React, {ChangeEvent} from 'react';
+import {render, screen, fireEvent} from 'test-utils';
+import {PaginationBar} from '.';
+import {PaginationProps} from '../../interfaces';
 
 describe('PaginationBar', () => {
-  const props = {
+  let props: PaginationProps;
+
+  beforeEach(() => props = {
     current: 1,
     perPage: 10,
     length: 30,
     handleChange(e: ChangeEvent<unknown>, p: number) {
       props.current = p;
     }
-  }
-
-  beforeEach(() => props.current = 1);
+  });
 
   it('should render properly', () => {
-    render(<PaginationBar { ...props } />);
+    render(<PaginationBar {...props} />);
     expect(screen.getByText('1')).toBeInTheDocument();
   });
 
-  it('should swith pages to chosen', () => {
-    const { rerender } = render(<PaginationBar { ...props } />);
+  it('should switch pages to chosen', () => {
+    const {rerender} = render(<PaginationBar {...props} />);
     fireEvent.click(screen.getByText('3'));
-    rerender(<PaginationBar { ...props } />);
+    rerender(<PaginationBar {...props} />);
     expect(screen.getByText('3')).toHaveClass('Mui-selected');
   });
 
   it('should switch to prev/next', () => {
-    const { rerender } = render(<PaginationBar { ...props } />);
+    const {rerender} = render(<PaginationBar {...props} />);
     fireEvent.click(screen.getByLabelText('Go to next page'));
-    rerender(<PaginationBar { ...props } />);
+    rerender(<PaginationBar {...props} />);
     expect(screen.getByText('2')).toHaveClass('Mui-selected');
 
     fireEvent.click(screen.getByLabelText('Go to previous page'));
-    rerender(<PaginationBar { ...props } />);
+    rerender(<PaginationBar {...props} />);
     expect(screen.getByText('1')).toHaveClass('Mui-selected');
-  })
+  });
 });
