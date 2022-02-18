2d0d85c22bdaa1e67b933be3c8e4673cf61baa32
Andriy Vitrenko <andriy.v@codegenerationsoftware.com>
2022-02-18 19:57:43 +0200

    added mock data to Feed page

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,20 +1,48 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Box } from '@mui/material';
 import { PostItem } from 'entities/PostItem';
 import { PaginationBar } from 'features/PaginationBar'; 
-import { getFeed } from 'shared/api/feed';
-import { Store } from 'shared/interfaces';
+// import { getFeed } from 'shared/api/feed';
+// import { Store } from 'shared/interfaces';
 import { PageTitle } from './Feed.styled';
+import {Post} from '../../shared/interfaces';
+
+const example: Post = {
+  commentCount: 999,
+  id: '1',
+  diggCount: 111,
+  text: 'test text',
+  hashtags: [{
+    id: '3',
+    name: 'test hashtag'
+  }],
+  videoUrl: 'test url',
+  authorMeta: {
+    id: '4',
+    avatar: 'avatar url',
+    name: 'john',
+    nickName: 'Johnny'
+  }
+};
+
+const feed = new Array(30);
+for (let i = 0; i < feed.length; i++) {
+  feed[i] = {
+    ...example,
+    id: Math.random().toString()
+  };
+}
 
 export const Feed: React.FC = () => {
   const dispatch = useDispatch();
-  const { feed } = useSelector((store: Store) => store);
+  // const { feed } = useSelector((store: Store) => store);
+  console.log(feed);
   const [ page, setPage ] = useState<number>(0);
   const PER_PAGE = 10;
 
   useEffect(() => {
-    dispatch(getFeed());
+    // dispatch(getFeed());
   }, [dispatch]);
 
   const handlePagination = (e: ChangeEvent<unknown>, p: number) => {
